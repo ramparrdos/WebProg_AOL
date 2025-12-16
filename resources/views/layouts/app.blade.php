04cb09412da1d39f7124b5f4319bbc7d5e165a4c
Jason <132189550+Jezter0@users.noreply.github.com>
2025-12-16 13:25:30 +0700

    Removed app.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -121,6 +121,5 @@
         @yield('content')
     </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
